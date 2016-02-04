7252742012f4a85cee22b32ed26e29bdadd40c9b
dumperize <ole.elena@gmail.com>
2016-02-05 00:03:50 +0400

    #375: Подогнаны данные для вкладки компании на странице рубрика каталога мастеров

--- catberry_components/_common/common-list/index.js
+++ catberry_components/_common/common-list/index.js
@@ -23,7 +23,11 @@ function CommonList() {
  * for template engine.
  */
 CommonList.prototype.render = function () {
-    return this.$context.getStoreData();
+    return this.$context.getStoreData()
+        .then(function(data){
+            console.log(data);
+            return data
+        });
 };
 
 /**
--- catberry_stores/master/MasterList.js
+++ catberry_stores/master/MasterList.js
@@ -39,7 +39,7 @@ function MasterList($uhr) {
  */
 MasterList.prototype.load = function () {
     var self = this;
-
+    console.log("dfsdf");
     return this.$context.getStoreData('Tag')
         .then(function (tag) {
             if (!tag.rubrika)
@@ -50,10 +50,10 @@ MasterList.prototype.load = function () {
             if (tag.tag.id) {
                 self._path = self._pathBase + '/bytag/' + tag.tag.id;
             }
-
             return self._loadDataPerPage(self._currentPage);
         })
         .then(function (result) {
+            console.log(result);
             if (!result || result.length === 0) {
                 self._isFinished = true;
                 return self._currentFeed;
@@ -64,6 +64,7 @@ MasterList.prototype.load = function () {
             return self._currentFeed;
         });
 };
+
 MasterList.prototype._clearFeed = function (tag) {
     this._currentRubrika = this._currentRubrika || tag.rubrika.id;
     this._currentTag = this._currentTag || tag.tag.id;
