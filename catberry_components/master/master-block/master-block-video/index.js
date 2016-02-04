2f0c72c18d869916dece060d281083075220b2be
old_graff <old_graff@rambler.ru>
2016-02-04 16:11:01 +0400

    fix video

--- catberry_components/master/master-block/master-block-video/index.js
+++ catberry_components/master/master-block/master-block-video/index.js
@@ -26,7 +26,7 @@ MasterBlockVideo.prototype.render = function () {
     if (this.$context.attributes['master-page']) {
         return this.$context.getStoreData()
             .then(function (data) {
-                console.log(data.videos);
+                //console.log(data.videos);
                 return {
                     videos: data.videos
                 }
@@ -40,13 +40,29 @@ MasterBlockVideo.prototype.render = function () {
  * @returns {Promise<Object>|Object|null|undefined} Binding settings.
  */
 MasterBlockVideo.prototype.bind = function () {
-    var video = $('.video-md__video-cover');
-    video.bind('click', showVideo);
+    $('.video-cont__video-cover').bind('click', showVideoPopup);
 
-    function showVideo() {
-        $.fancybox.open(this, {
-            padding: 0,
-            type: 'iframe',
+    function showVideoPopup() {
+        //console.log('click!');
+        var tmp = $('.popup-video-cont').clone(),
+            url = $(this).attr('href'),
+            title = $(this).find('.video-cont__title-prev').html(),
+            desc = $(this).find('.video-cont__desc').html(),
+            name = $('.master-card-mp__name').html(),
+            imgSrc = $('.master-page__img').attr('src');
+        tmp.find('.popup-video-cont__video-cont').html('<iframe src="' + url + '" frameborder="0" allowfullscreen></iframe>');
+        tmp.find('.popup-video-cont__title').html(title);
+        tmp.find('.popup-video-cont__text').html(desc);
+        tmp.find('.master-micro-inf__name').html('<br>' + name);
+        tmp.find('.master-micro-inf img').attr('src', imgSrc);
+        $.fancybox.open(tmp, {
+            padding: 20,
+            type: 'inline',
+            width: '80%',
+            maxWidth: '800px',
+            minWidth: '250px',
+            autoHeight: true,
+            autoSize: false,
             helpers: {
                 overlay: {
                     locked: false
