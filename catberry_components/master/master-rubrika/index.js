0a9f08c7645f0967b05223e287e8fac5536594df
old_graff <old_graff@rambler.ru>
2016-02-08 12:36:04 +0400

    fix video

--- catberry_components/master/master-rubrika/index.js
+++ catberry_components/master/master-rubrika/index.js
@@ -23,7 +23,11 @@ function MasterRubrika() {
  * for template engine.
  */
 MasterRubrika.prototype.render = function () {
-    return this.$context.getStoreData();
+    return this.$context.getStoreData()
+        .then(function (data) {
+            console.log(data);
+            return data;
+        });
 };
 
 /**
