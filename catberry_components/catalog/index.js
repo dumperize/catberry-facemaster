d99a729800eea53a886b7d9a7d63f56012074e18
old_graff <old_graff@rambler.ru>
2016-02-09 11:53:03 +0400

    #428

--- catberry_components/catalog/index.js
+++ catberry_components/catalog/index.js
@@ -26,7 +26,7 @@ Video.prototype.render = function () {
     return this.$context.getStoreData()
         .then(function (data) {
             data.catalog = self.$context.attributes.catalog;
-            console.log(data);
+            //console.log(data);
             return data;
         });
 };
