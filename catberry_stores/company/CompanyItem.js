f89ab57e4cfe215c40a5d0c5712836ef5c5cecfc
dumperize <ole.elena@gmail.com>
2016-02-11 10:40:31 +0400

    #438: данные для страницы компании

--- catberry_stores/company/CompanyItem.js
+++ catberry_stores/company/CompanyItem.js
@@ -28,7 +28,7 @@ function CompanyItem($uhr) {
     this._options = {
         data: {
             filter: '["and", ["=","number", ":number"]]',
-            expand: "master,mastersData,videos,albums,contacts,schedule,workCondition"
+            expand: "master,masters,videos,albums,contacts,schedule,workCondition"
         }
     };
 }
@@ -40,15 +40,59 @@ function CompanyItem($uhr) {
 CompanyItem.prototype.load = function () {
     var self = this;
     var id = this.$context.state.id;
+    var company = [];
     if (!id)
         return;
 
     this._optionsData.data.filter[':number'] = id;
     return this._load()
         .then(function (result) {
-            if (result.content.length == 0 || result.content[0].mastersData.count_masters == 0)
+            if (result.content.length == 0 || result.content[0].masters.count_masters == 0)
                 self.$context.notFound();
-            return result.content[0];
+            company = result.content[0];
+            return company;
+        })
+        .then(function (company) {
+            var listID = [];
+            company.masters.forEach(function (master) {
+                listID.push(master.id);
+            });
+            return self.$context.sendAction("master/MasterListForCompany", "setListID", listID);
+        })
+        .then(function (r) {
+            return self.$context.getStoreData('master/MasterListForCompany');
+        })
+        .then(function(masters){
+            company.masters = masters;
+            company.isBlock = {
+                masters: {
+                    access: true,
+                    name: "Мастера",
+                    active: true
+                },
+                work: {
+                    access: true,
+                    name: "Условия работы",
+                    active: (company.schedule.length > 0)
+                },
+                about: {
+                    access: true,
+                    name: "О себе",
+                    active: (company.about)
+                },
+                photo: {
+                    access: true,
+                    name: "Фото",
+                    active: (company.albums.length > 0)
+                },
+                video: {
+                    access: true,
+                    name: "Видео",
+                    active: (company.videos.length > 0)
+                }
+            };
+
+            return company;
         });
 };
 
