db95d0a71f98a10cdf09fbf4aed11de70d41f693
old_graff <old_graff@rambler.ru>
2016-02-18 14:25:24 +0400

    fix svg fix fancybox

--- catberry_components/master/master-minicard/master-minicard/index.js
+++ catberry_components/master/master-minicard/master-minicard/index.js
@@ -81,9 +81,9 @@ MasterMinicard.prototype.render = function () {
  * @returns {Promise<Object>|Object|null|undefined} Binding settings.
  */
 MasterMinicard.prototype.bind = function () {
-    var width = window.innerWidth
-        || document.documentElement.clientWidth
-        || document.body.clientWidth;
+    var width = document.documentElement.clientWidth
+        || document.body.clientWidth
+        || window.innerWidth;
 
     this._minicardServicesCut();
     this._window.addEventListener('resize', this._minicardServicesCut.bind(this));
@@ -110,8 +110,11 @@ MasterMinicard.prototype.handleShowWidgetTab = function (event) {
     event.preventDefault();
     event.stopPropagation();
     var targetElement = event.currentTarget;
+    var width = document.documentElement.clientWidth
+        || document.body.clientWidth
+        || window.innerWidth;
 
-    if ($(targetElement).hasClass('act') && ($(window).width() >= 750)) {
+    if ($(targetElement).hasClass('act') && (width >= 750)) {
         $(targetElement).siblings().removeClass('show');
         $(targetElement).addClass('show');
     }
@@ -143,9 +146,9 @@ MasterMinicard.prototype.handleShowService = function (event) {
  * @private
  */
 MasterMinicard.prototype._minicardServicesCut = function () {
-    var width = window.innerWidth
-        || document.documentElement.clientWidth
-        || document.body.clientWidth;
+    var width = document.documentElement.clientWidth
+        || document.body.clientWidth
+        || window.innerWidth;
 
     var carcas = this.$context.element.querySelector('.master-minicard');
     var services = this.$context.element.querySelector('.master-minicard__services');
